17957c21f57f143cd132ac8f725eedd52a81a071
grig0076 <susie.grigson@flinders.edu.au>
2024-08-07 16:36:53 +0930

    update loss function

--- src/model_onehot.py
+++ src/model_onehot.py
@@ -34,7 +34,7 @@ class VariableSeq2SeqEmbeddingDataset(Dataset):
         
         # select a random cateogory to mask if training 
         if self.training: 
-            masked_category = self.category_mask(category)
+            masked_category, idx = self.category_mask(category)
         else: 
             masked_category = category
             
@@ -45,8 +45,8 @@ class VariableSeq2SeqEmbeddingDataset(Dataset):
         embedding_one_hot = torch.tensor(np.hstack((one_hot, embedding)))
      
         self.embedding = embedding_one_hot
-
-        return embedding_one_hot, category, mask 
+  
+        return embedding_one_hot, category, mask, idx 
     
     def set_training(self, training=True):
         self.training = training
@@ -62,7 +62,7 @@ class VariableSeq2SeqEmbeddingDataset(Dataset):
         masked_category = category.clone()
         masked_category[idx] = self.mask_token
         
-        return masked_category
+        return masked_category, idx
 
     def shuffle_rows(self):
         """
@@ -124,13 +124,14 @@ class VariableSeq2SeqTransformerClassifier(nn.Module):
         return pos_enc.unsqueeze(0)
 
 def collate_fn(batch):
-    embeddings, categories, masks = zip(*batch)
+    embeddings, categories, masks, idx = zip(*batch)
     embeddings_padded = pad_sequence(embeddings, batch_first=True)
     categories_padded = pad_sequence(categories, batch_first=True, padding_value=-1)
     masks_padded = pad_sequence(masks, batch_first=True, padding_value=0)
-    return embeddings_padded, categories_padded, masks_padded
+    return embeddings_padded, categories_padded, masks_padded, idx 
  
-def masked_loss(output, target, mask, ignore_index=-1):
+def loss(output, target, mask, ignore_index=-1):
+    # this loss function should be looking at the predicting gene not everything 
     target = target.clone()
     target[mask == 0] = ignore_index
     loss_fct = nn.CrossEntropyLoss(ignore_index=ignore_index, reduction='none').cuda()
@@ -138,6 +139,13 @@ def masked_loss(output, target, mask, ignore_index=-1):
     loss = loss * mask.view(-1)
     return loss.sum() / mask.sum()
 
+def masked_loss(output, target, mask, idx, ignore_index=-1):
+    # this loss function returns the loss just for the prediction at hand 
+    target = target.clone()
+    target[mask == 0] = ignore_index
+    loss_fct = nn.CrossEntropyLoss(ignore_index=ignore_index, reduction='none').cuda()
+    loss = loss_fct(output.view(-1, output.size(-1)), target.view(-1))
+    return loss[idx]
 
 def train(model, train_dataloader, test_dataloader, epochs=5, lr=1e-5, save_path='model', device='cuda'):
     print('Training on ' + str(device), flush=True)
@@ -151,12 +159,12 @@ def train(model, train_dataloader, test_dataloader, epochs=5, lr=1e-5, save_path
     print('Beginning training loop', flush=True)
     for epoch in range(epochs):
         total_loss = 0
-        for embeddings, categories, masks in train_dataloader:
+        for embeddings, categories, masks, idx in train_dataloader:
             embeddings, categories, masks = embeddings.to(device).float(), categories.to(device).long(), masks.to(device).float()
             optimizer.zero_grad()
             src_key_padding_mask = (masks == 0)  # Mask for transformer
             outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
-            loss = masked_loss(outputs, categories, masks)
+            loss = masked_loss(outputs, categories, masks, idx) # need to specify which index to reference in the loss function
             loss.backward()
             optimizer.step()
             
@@ -174,11 +182,11 @@ def train(model, train_dataloader, test_dataloader, epochs=5, lr=1e-5, save_path
         model.eval()
         total_val_loss = 0
         with torch.no_grad():
-            for embeddings, categories, masks in test_dataloader:
+            for embeddings, categories, masks, idx in test_dataloader:
                 embeddings, categories, masks = embeddings.to(device).float(), categories.to(device).long(), masks.to(device).float()
                 src_key_padding_mask = (masks == 0)  # Mask for transformer
                 outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
-                val_loss = positional_loss(outputs, categories, masks, model.positional_encoding)
+                val_loss = masked_loss(outputs, categories, masks, idx) # need to include idx in the loss function 
                 total_val_loss += val_loss.item()
         
         avg_val_loss = total_val_loss / len(test_dataloader)
@@ -244,213 +252,66 @@ def train_crossValidation(dataset, phrog_integer, n_splits=10, batch_size=16, ep
         torch.cuda.empty_cache()
 
         # Evaluate performance for this kfold
-        evaluate_with_optimal_thresholds(kfold_transformer_model, val_kfold_loader, phrog_integer, device, output_dir=output_dir)
+        evaluate(kfold_transformer_model, val_kfold_loader, phrog_integer, device, output_dir=output_dir)
         
         fold += 1
 
-def evaluate(model, dataloader):
-    model.eval()
-    correct = 0
-    total = 0
-    
-    counter = 0 
-    with torch.no_grad():
-        for embeddings, categories, masks in dataloader:
-            
-            src_key_padding_mask = (masks == 0)  # Mask for transformer
-            outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
-            _, predicted = torch.max(outputs, 2) # 2 referes to the vlaue 
-            
-            
-            counter += 1 
-            
-            valid = masks.byte() # specify which predictions are not masked 
-            correct += (predicted == categories).masked_select(valid).sum().item()
-            #print((predicted == categories).masked_select(valid).sum().item())
-            total += valid.sum().item()
-            
-    accuracy = correct / total if total > 0 else 0
-    print(f"Accuracy: {accuracy:.4f}")
-    print("Total predictions: " + str(total))
-
-def evaluate_with_threshold(model, dataloader, threshold=0.5):
-    model.eval()
-    correct = 0
-    total_confident_predictions = 0
-    
-    with torch.no_grad():
-        for embeddings, categories, masks in dataloader:
-            embeddings = embeddings.float()
-            src_key_padding_mask = (masks == 0)  # Mask for transformer
-            outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
-            
-            # Apply softmax to get probabilitilsls
-            es
-            probs = F.softmax(outputs, dim=2)
-            max_probs, predicted = torch.max(probs, dim=2)
-            
-            # Apply thresholding
-            confident_predictions = max_probs >= threshold
-            
-            # Mask for valid and confident predictions
-            valid = masks.byte()
-            confident_and_valid = confident_predictions & valid
-            
-            correct += (predicted == categories).masked_select(confident_and_valid).sum().item()
-            total_confident_predictions += confident_and_valid.sum().item()
-    
-    accuracy = correct / total_confident_predictions if total_confident_predictions > 0 else 0
-    print(f"Accuracy: {accuracy:.4f}")
-    print(f"Confident Predictions: {total_confident_predictions}")
-
-def evaluate_with_metrics_and_save(model, dataloader, threshold=0.5, output_dir='metrics_output'):
-    model.eval()
-    all_labels = []
-    all_preds = []
-    all_probs = []
-    
-    with torch.no_grad():
-        for embeddings, categories, masks in dataloader:
-            embeddings = embeddings.float()
-            src_key_padding_mask = (masks == 0)  # Mask for transformer
-            outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
-            
-            # Apply softmax to get probabilities
-            probs = F.softmax(outputs, dim=2)
-            max_probs, predicted = torch.max(probs, dim=2)
-            
-            valid = masks.byte()
-            confident_predictions = max_probs >= threshold
-            
-            # Flatten arrays and filter out padding
-            valid = valid.view(-1)
-            probs = probs.view(-1, probs.size(-1)).cpu().numpy()
-            predicted = predicted.view(-1).cpu().numpy()
-            categories = categories.view(-1).cpu().numpy()
-            
-            confident_indices = confident_predictions.view(-1).cpu().numpy().astype(bool)
-            valid_indices = valid.cpu().numpy().astype(bool)
-            mask = confident_indices & valid_indices
-            
-            all_labels.extend(categories[mask])
-            all_preds.extend(predicted[mask])
-            all_probs.extend(probs[mask])
-    
-    all_labels = np.array(all_labels)
-    all_preds = np.array(all_preds)
-    all_probs = np.array(all_probs)
-    
-    # Compute F1 score, precision, recall for each category
-    f1 = f1_score(all_labels, all_preds, average=None)
-    precision = precision_score(all_labels, all_preds, average=None)
-    recall = recall_score(all_labels, all_preds, average=None)
-    
-    # Save F1, precision, recall to CSV
-    metrics_df = pd.DataFrame({'Category': np.arange(len(f1)), 'F1': f1, 'Precision': precision, 'Recall': recall})
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    metrics_df.to_csv(os.path.join(output_dir, 'metrics.csv'), index=False)
-    
-    # Save ROC curve data to CSV
-    num_classes = all_probs.shape[1]
-    for i in range(num_classes):
-        fpr, tpr, _ = roc_curve(all_labels == i, all_probs[:, i])
-        roc_auc = auc(fpr, tpr)
-        roc_df = pd.DataFrame({'FPR': fpr, 'TPR': tpr})
-        roc_df.to_csv(os.path.join(output_dir, f'roc_curve_category_{i}.csv'), index=False)
-        print(f"Category {i} ROC AUC: {roc_auc:.4f}")
-
-    print(f"F1 Scores per category: {f1}")
-    print(f"Precision per category: {precision}")
-    print(f"Recall per category: {recall}")
-
-    # Modified evaluation function to include ROC curve and metrics calculation
-
-def evaluate_with_optimal_thresholds(model, dataloader, phrog_integer, device, output_dir='metrics_output'):
+def evaluate(model, dataloader, phrog_integer, device, output_dir='metrics_output'):
     """
     Threshold is selected that optimizes Youden's J index 
     """
     model.to(device)
     model.eval()
+    labels = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     all_labels = []
     all_probs = []
+    all_preds = []
     
-
-    # TODO - this evaluate is incorrect - work through this 
     with torch.no_grad():
-        for embeddings, categories, masks in dataloader:
+        for embeddings, categories, masks, idx in dataloader:
             embeddings, categories, masks = embeddings.to(device).float(), categories.to(device).long(), masks.to(device).float()
             src_key_padding_mask = (masks == 0)  # Mask for transformer
             outputs = model(embeddings, src_key_padding_mask=src_key_padding_mask)
             
             # Apply softmax to get probabilities
             probs = F.softmax(outputs, dim=2)
-            max_probs, predicted = torch.max(probs, dim=2)
-            
-            valid = masks.byte()
-            
-            # Flatten arrays and filter out padding
-            valid = valid.view(-1)
-            probs = probs.view(-1, probs.size(-1)).cpu().numpy()
-            predicted = predicted.view(-1).cpu().numpy()
-            categories = categories.view(-1).cpu().numpy()
-            
-            valid_indices = valid.cpu().numpy().astype(bool)
-            
-            all_labels.extend(categories[valid_indices])
-            all_probs.extend(probs[valid_indices])
+            probs_idx = probs.detach().cpu().numpy()[0][idx][0]
+
+            all_labels.append(categories.detach().cpu().numpy()[0][idx][0])
+            all_probs.append(probs_idx)
+            all_preds.append(np.argmax(probs_idx))
     
     all_labels = np.array(all_labels)
     all_probs = np.array(all_probs)
-
-    # Ensure there are predictions to evaluate
-    if len(all_labels) == 0 or len(all_probs) == 0:
-        print("No predictions to evaluate.")
-        return
     
-    labels = list(set(all_labels))
     optimal_thresholds = np.zeros(len(labels))
     retained_proportion = np.zeros(len(labels))
 
-    for idx, label in enumerate(labels):
-        fpr, tpr, thresholds = roc_curve(all_labels == label, all_probs[:, idx])
+    for ii, label in enumerate(labels):
+        roc_probs = [i[ii] for i in all_probs] 
+        roc_labels = [1 if i ==ii else 0 for i in all_labels] 
+        fpr, tpr, thresholds = roc_curve(roc_labels, roc_probs)
         roc_auc = auc(fpr, tpr)
-
-        # Find the optimal threshold using Youden's J statistic (maximizing (sensitivity + specificity - 1))
-        optimal_idx = np.argmax(tpr - fpr)
-        optimal_thresholds[idx] = thresholds[optimal_idx]
-
-        # Calculate the retained proportion of predictions above the optimal threshold
-        total_predictions = len(all_probs[:, idx])
-        retained_predictions = np.sum(all_probs[:, idx] >= optimal_thresholds[idx])
-        retained_proportion[idx] = retained_predictions / total_predictions
+        print('AUC for category '+ phrog_integer.get(label) + ': ' + str(roc_auc))
 
         roc_df = pd.DataFrame({'FPR': fpr, 'TPR': tpr, 'Thresholds': thresholds})
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         roc_df.to_csv(os.path.join(output_dir, f'roc_curve_category_{phrog_integer.get(label)}.csv'), index=False)
-        print(f"Category: {phrog_integer.get(label)} \n\tROC AUC: {roc_auc:.4f}, Optimal Threshold: {optimal_thresholds[idx]:.4f}")
 
-    # Use optimal thresholds to make final predictions
-    final_preds = (all_probs >= optimal_thresholds).astype(int)
-    
+
     # Compute F1 score, precision, recall for each category using optimal thresholds
-    f1 = f1_score(all_labels, final_preds.argmax(axis=1), average=None, zero_division=1)
-    precision = precision_score(all_labels, final_preds.argmax(axis=1), average=None, zero_division=1)
-    recall = recall_score(all_labels, final_preds.argmax(axis=1), average=None, zero_division=1)
-    
+    f1 = f1_score(all_labels, all_preds, average=None, zero_division=1)
+    precision = precision_score(all_labels, all_preds, average=None, zero_division=1)
+    recall = recall_score(all_labels, all_preds, average=None, zero_division=1)
+        
     # Save F1, precision, recall to CSV
     metrics_df = pd.DataFrame({
-        'Category': [phrog_integer.get(label) for label in labels], 
-        'F1': f1, 
-        'Precision': precision, 
-        'Recall': recall, 
-        'Optimal Threshold': optimal_thresholds,
-        'Proportion Retained': retained_proportion
-    })
+            'Category': [phrog_integer.get(label) for label in labels], 
+            'F1': f1, 
+            'Precision': precision, 
+            'Recall': recall, 
+            'Optimal Threshold': optimal_thresholds,
+            'Proportion Retained': retained_proportion
+        })
     metrics_df.to_csv(os.path.join(output_dir, 'metrics_with_optimal_thresholds.csv'), index=False)
-
-    print(f"F1 Scores per category: {f1}")
-    print(f"Precision per category: {precision}")
-    print(f"Recall per category: {recall}")
-    print(f"Proportion of predictions retained: {retained_proportion}")
